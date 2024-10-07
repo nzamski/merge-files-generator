f3c159fb13498bfe976b4e94770f01d372420bf8
נעם זמסקי <u9264173@bsmch.net>
2024-10-07 09:12:49 +0300

    fix(index.js): fix pkg command bug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const path = require("path");
 const prompts = require("prompts");
-const { exec } = require("child_process");
+const { exec } = require("pkg");
 const questions = require("./questions");
 
 (async () => {
@@ -11,17 +11,15 @@ const questions = require("./questions");
 
   fs.writeFileSync(path.join(__dirname, "config.json"), JSON.stringify(config, null, 2));
 
-  exec(
-    `npx pkg -t ${targets.join(
-      ","
-    )} --out-path=${process.cwd()} merge-files.js`,
-    { cwd: __dirname },
-    (error) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-      } else {
-        console.log("Executable merge-files generated successfully");
-      }
-    }
-  );
+  exec([
+    path.resolve(__dirname, 'merge-files.js'),
+    '--out-path', process.cwd(),
+    '-t', targets.join(',')
+  ])
+    .then(() => {
+      console.log("Executable merge-files generated successfully");
+    })
+    .catch((error) => {
+      console.error(`exec error: ${error}`);
+    });
 })();
